b4a29e0b2fa450a14bc2da33b0a351e907abe566
matheusponte <matheuspontes894@outlook.com>
2024-05-20 20:31:51 -0300

    Criado atributos e metodo Create

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText valorDoAtributo;
+    private Spinner medida1, medida2;
+    private Button conversor;
+    private TextView resultado;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle instanciaSalva) {
+        super.onCreate(instanciaSalva);
         setContentView(R.layout.activity_main);
+
+            valorDoAtributo = findViewById(R.id.input_valor);
+            medida1 = findViewById(R.id.spinner_unidade1);
+            medida2 = findViewById(R.id.spinner_unidade2);
+            conversor = findViewById(R.id.convert_button);
+            resultado = findViewById(R.id.result_text);
 
         }
     }
